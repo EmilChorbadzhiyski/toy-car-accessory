4518270246571a2dcdd93edef4601a55d2f06bf4
Emil Chorbadzhiyski <145546471+EmilChorbadzhiyski@users.noreply.github.com>
2025-03-22 22:54:36 +0200

    Add validation constraints to Accessory_Request fields

--- src/main/java/app/web/dto/AccessoryRequest.java
+++ src/main/java/app/web/dto/AccessoryRequest.java
@@ -1,13 +1,18 @@
 package app.web.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
 public class AccessoryRequest {
+    @NotBlank
     private String accessoryName;
+    @NotBlank
     private String description;
+    @NotBlank
     private BigDecimal price;
+    @NotBlank
     private String imageUrl;
 }
